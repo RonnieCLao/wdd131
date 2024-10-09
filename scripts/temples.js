9dd09604268010a894a56ea30b9390ba99f94e37
Ronnie Lao <lao21001@byui.edu>
2024-10-09 01:02:18 -0600

    temple album touch-up

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,5 +1,5 @@
 document.getElementById('hamburger-menu').addEventListener('click', function() {
-    var menu = document.getElementById('sidebar-menu');
+    var menu = document.getElementById('mini-menu');
     if (menu.style.display === 'flex') {
         menu.style.display = 'none';
     } else {
